6aa55b1e291bdeb2895d926aa8e2d0db4d6bbca7
vadimnovikovgit <vadimnovikov666@gmail.com>
2020-02-19 10:28:54 +0300

    cleate class logger and outputtype

--- Logger/headers/logger.h
+++ Logger/headers/logger.h
@@ -1,1 +1,29 @@
 #pragma once
+
+
+namespace alphaLogger
+{
+	class output
+	{
+	public:
+		output()
+		{
+			m_outputType = 1;
+		}
+		output(enum t) :
+			m_outputType(t)
+	private:
+		enum m_outputType{"File","Console","FC","Stream"};
+	};
+
+
+
+	class Logger
+	{
+	public:
+		Logger();
+
+	private:
+		output* out;
+	};
+}
